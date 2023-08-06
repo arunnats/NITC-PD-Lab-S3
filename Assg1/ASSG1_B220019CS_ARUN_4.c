807e17a3a25b0c3cfca3082500c1e625c25a5ca6
arunnats <arunnats2004@gmail.com>
2023-08-06 20:47:09 +0530

    added assignment 4 coded some basic loops

---
+++ Assg1/ASSG1_B220019CS_ARUN_4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+#define LENGTH 200
+
+void main()
+{
+    char Str[LENGTH], fancyStr[LENGTH];
+    scanf("%[^\n]", Str); 
+    //printf("%s", Str);
+    int i,j,n=0,k=0;
+    while(Str[n]!='\0')
+    {
+             
+             n++;
+    }
+    
+}   
