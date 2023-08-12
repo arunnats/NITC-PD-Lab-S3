e90e0c4d0caf5b7b054d17b931e60514629199de
arunnats <arunnats2004@gmail.com>
2023-08-12 15:15:13 +0530

    finished 4th qustion

---
+++ Assg2/ASSG2_B220019CS_ARUN/ASS2_B220019CS_ARUN_4.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int linear(int arr[],int key, int n)
+{
+    int i=0;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==key)
+        {
+            return i;  
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int n, key, i;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    scanf("%d",&key);
+
+    int ans = linear(arr, key, n);
+    
+    printf("%d", ans);
+    return 0;
+}
